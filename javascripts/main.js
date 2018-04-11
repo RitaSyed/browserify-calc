9fb469f9b169c226f2b7148cabe1912c5f5967ca
Rita Patrick <rita.patrick1007@gmail.com>
2018-04-10 20:13:53 -0500

    done

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,8 @@
-
-console.log('hi');
+const add = require('./add');
+const substract = require('./substract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+console.log('Adding:', add(1, 2));
+console.log('Substracting:', substract(40, 5));
+console.log('Multiply:', multiply(5, 5));
+console.log('Divide:', divide(40, 2));
